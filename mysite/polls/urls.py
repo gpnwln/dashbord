873b4656c74595bf61721b7371d1260a386ab72d
gpnwln <gapanovichvalentin@gmail.com>
2023-04-03 23:01:39 +0300

    show

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('indexht/', views.indexht, name='indexht'),
+    path('indexhtml/', views.indexnew, name='indexhtml'),
     path('<str:question_id>/', views.detail, name='detail'),
     path('<str:question_id>/results/', views.results, name='results'),
     path('<str:question_id>/vote/', views.vote, name='vote'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,31 @@
 from django.http import HttpResponse
-#response - ответ, request - запрос
+# response - ответ, request - запрос
 # from .models import Question
+
+from django.shortcuts import render
+from django.template import loader
+
+
+def indexnew(request):
+    template = loader.get_template("template/polls/index.html")
+    context = "Ready html"
+    return HttpResponse(template.render(context, request))
+
+def indexht(request):
+    return render(request, "index.html")
 
 def index(request):
     return HttpResponse("Hello, world. You're at the pools")
 
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
