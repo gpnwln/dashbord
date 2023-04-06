a1e3c3b46e78e3de98a71a08a5fac6b3e51c0bc0
gpnwln <gapanovichvalentin@gmail.com>
2023-04-06 17:45:34 +0300

    admin, 2 links

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "study",
+        "USER": "myuser",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('indexht/', views.indexht, name='indexht'),
-    path('indexhtml/', views.indexnew, name='indexhtml'),
-    path('<str:question_id>/', views.detail, name='detail'),
-    path('<str:question_id>/results/', views.results, name='results'),
-    path('<str:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
