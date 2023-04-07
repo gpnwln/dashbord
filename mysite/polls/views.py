78df08a5071957f269a6b880890bdf20d939eb94
gpnwln <gapanovichvalentin@gmail.com>
2023-04-07 13:01:07 +0300

    Переходы

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -25,3 +25,9 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def firstpage(request):
+    return render(request, "polls/startpage.html")
+
+def secondpage(request):
+    return render(request, "polls/secondpage.html")
